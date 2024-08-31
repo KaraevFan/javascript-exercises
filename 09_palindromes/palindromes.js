86aa037e5869275e03dc3432624ce9c8a399cbca
KaraevFan <tom.n.kano@gmail.com>
2024-08-31 14:55:20 +0900

    Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+/*
+get version of string with no lowercaps, punctuations, whitespaces
+check if cleaned string is same as reversed
+*/
 
+const palindromes = function (str) {
+    phraseCleaned = str.replace(/[^\w]/g, '').toLowerCase();
+    return phraseCleaned == phraseCleaned.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
-};
+function findTheOldest(peopleArray) {
+    const currentYear = new Date().getFullYear();
+    
+    return peopleArray.reduce((oldest, current) => {
+      const oldestAge = oldest.yearOfDeath 
+        ? oldest.yearOfDeath - oldest.yearOfBirth
+        : currentYear - oldest.yearOfBirth;
+      
+      const currentAge = current.yearOfDeath
+        ? current.yearOfDeath - current.yearOfBirth
+        : currentYear - current.yearOfBirth;
+      
+      return currentAge > oldestAge ? current : oldest;
+    });
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
